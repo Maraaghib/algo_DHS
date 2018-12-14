e36f5d01e916034aa2be45f951ac7dc4174ae412
louis <louis.demoulins@etu.u-bordeaux.fr>
2018-12-14 12:37:07 +0100

    Avancement sur les handlers de messages

--- src/Main/BasicNode.java
+++ src/Main/BasicNode.java
@@ -1,6 +1,6 @@
 package Main;
 
-import Main.MessageObjects.FloodMessage;
+import Main.MessageObjects.*;
 import jbotsim.Message;
 import jbotsim.Node;
 
@@ -21,6 +21,8 @@ public class BasicNode extends Node {
     private Vector<Integer> sonsInFragment;
     private Vector<Integer> border;
     private Vector<Integer> readys;
+
+    private boolean fragSent = false;
     @Override
     public void onStart() {
         if(getID() == 0)
@@ -47,6 +49,14 @@ public class BasicNode extends Node {
 
         if(content.getClass() == FloodMessage.class)
             handleFlood((FloodMessage) content);
+        else if(content.getClass() == FragMessage.class)
+            handleFrag((FragMessage) content, message.getSender());
+        else if(content.getClass() == PulseMessage.class)
+            handlePulse((PulseMessage) content);
+        else if(content.getClass() == SyncMessage.class)
+            handleSync((SyncMessage) content);
+        else if(content.getClass() == MinMessage.class)
+            handleMin((MinMessage) content);
 
     }
 
@@ -72,5 +82,45 @@ public class BasicNode extends Node {
             fatherT = msg.father;
             sendAll(new FloodMessage(getID()));
         }
+    }
+
+    private void handleFrag(FragMessage msg, Node sender)
+    {
+        if(!fragSent)
+        {
+            send(sender, new FragMessage(currentFragment));
+            fragSent = true;
+        }
+        border.add(msg.fragmentRoot);
+        //TODO : calculer la distance pour calculer le BEST
+        if(best < 0 /*|| dist < best*/)
+        {}
+    }
+
+    private void handlePulse(PulseMessage msg)
+    {
+        fragSent = false;
+        for (Node n: getOutNeighbors()) {
+            for (Integer i : sonsInFragment)
+                if (!i.equals(n.getID()) || !(fatherInFragment == n.getID()))
+                    send(n, new FragMessage(currentFragment));
+        }
+        fragSent = true;
+    }
+
+    private void handleSync(SyncMessage msg)
+    {
+        if(fatherInFragment == getID())
+        {
+            for (Node n: getOutNeighbors())
+                for(Integer i : sonsInFragment)
+                    if(i.equals(n.getID()))
+                        send(n, new PulseMessage());
+        }
+    }
+
+    private void handleMin(MinMessage msg)
+    {
+
     }
 }
